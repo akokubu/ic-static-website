64af86065f4cc70be7af07327d4f3e72cfc52674
Atsushi Kokubu <akokubu@gmail.com>
2023-01-14 06:40:37 +0900

    icp

---
+++ src/lib/About.svelte
@@ -0,0 +1,23 @@
+<script>
+  import profileImage from "../assets/unchain_logo.png";
+</script>
+
+<section id="about">
+  <div class="py32 text-gray-600">
+    <h1 class="text-center mb-10 font-medium text-3xl">ABOUNT ME</h1>
+    <div class="flex flex-wrap lg:w-4/5 sm:mx-auto">
+      <div class="p-2 md:w-1/2 w-full">
+        <img src={profileImage} alt="" class="mx-auto mt-24 md:mt-0" />
+      </div>
+      <div class="flex flex-col text-center p-2 md:w-1/2 w-full">
+        <h1 class="text-gray-900 mb-10 font-medium">akokubu</h1>
+        <p class="mb-8 leading-relaxed">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eaque vitae
+          fugiat repudiandae exercitationem accusamus eos corrupti, fugit
+          voluptas debitis sint quis, magni vel quam eum, consequatur laboriosam
+          quas dolore? Voluptas.
+        </p>
+      </div>
+    </div>
+  </div>
+</section>
---
+++ src/lib/Portfolio.svelte
@@ -0,0 +1,27 @@
+<script>
+  let projects = [
+    { name: "ETH dApp", url: "" },
+    { name: "ETH NFT Collection", url: "" },
+    { name: "ETH NFT Maker", url: "" },
+    { name: "ETH NFT Game", url: "" },
+  ];
+</script>
+
+<section id="portfolio">
+  <div class="container mx-auto">
+    <div class="py32">
+      <h1 class="text-center mb-10 font-medium text-3xl">PORTFOLIO</h1>
+      <div class="flex flex-wrap lg:w-4/5 sm:mx-auto">
+        {#each projects as project}
+          <div class="p-2 md:w-1/3 w-full">
+            <div
+              class="bg-gray-400 text-wite hover:bg-gray-800 cursor-pointer rounded p-4 text-center"
+            >
+              <a href={project.url}>{project.name}</a>
+            </div>
+          </div>
+        {/each}
+      </div>
+    </div>
+  </div>
+</section>
